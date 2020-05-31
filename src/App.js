c40c187ec071958378287279db1dbd4df13e31dd
Zulfikar Ahmed <zulfikar.ahmed@gmail.com>
2020-05-31 01:00:16 -0500

    Added traits.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,171 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const ocean = [3, 3, 2, 2, 3];
+const { MongoClient } = require('mongodb');
 
 class VirtueList extends Component {
 
+  constructor() {
+    // connect to mongo and query
+    const uri = ""
+    const client = new MongoClient(uri);
 
-  render() {
-    return (
-      <div>
-        "In the darkest night of your life, these are the virtues that will assist you."
-      </div>
-    )
+    try {
+      await client.connect();
+      var collection = client.prod.uniqueness;
+      collection.findOne({ counter: vCounter },
+        function (err, item) {
+          this.props.profile = item
+        }
+      );
+
+    });
+  });
+} catch (e) {
+  console.error(e);
+}
+
   }
+
+list_of_virtues(){
+  vals = [
+    { props.profile.Assertiveness },
+    { props.profile.Captiousness },
+    { props.profile.Blunt },
+    { props.profile.SocialConfidence },
+    { props.profile.Forgiveness },
+    { props.profile.Stubbornness },
+    { props.profile.Narcissism },
+    { props.profile.Deviousness },
+    { props.profile.Consideration },
+    { props.profile.Empathy },
+    { props.profile.Introversion },
+    { props.profile.SocialDominance },
+    { props.profile.PeopleVsThings },
+    { props.profile.DisciplinedIsolation },
+    { props.profile.Affable },
+    { props.profile.Sociability },
+    { props.profile.SocialDependence },
+    { props.profile.IndependentMinded },
+    { props.profile.Aggression },
+    { props.profile.SensationSeeking },
+    { props.profile.StressResilience },
+    { props.profile.Antagonism },
+    { props.profile.Vigilance },
+    { props.profile.Optimism },
+    { props.profile.Surgency },
+    { props.profile.Vigor },
+    { props.profile.Frivolous },
+    { props.profile.Dysthymia },
+    { props.profile.EmotionalReactivity },
+    { props.profile.Negativity },
+    { props.profile.Callousness },
+    { props.profile.Psychopathy },
+    { props.vCounterprofile.ConflictedRelationships }
+  ]
+
+
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+
+
+  tnames = [
+      "Assertiveness",
+      "Captiousness",
+      "Blunt",
+      "SocialConfidence",
+      "Forgiveness",
+      "Stubbornness",
+      "Narcissism",
+      "Deviousness",
+      "Consideration",
+      "Empathy",
+      "Introversion",
+      "SocialDominance",
+      "PeopleVsThings",
+      "DisciplinedIsolation",
+      "Autonomy",
+      "Affable",
+      "Sociability",
+      "SocialDependence",
+      "IndependentMinded",
+      "Aggression",
+      "SensationSeeking",
+      "StressResilience",
+      "Antagonism",
+      "Vigilance",
+      "Optimism",
+      "Surgency",
+      "Vigor",
+      "Frivolous",
+      "Dysthymia",
+      "EmotionalReactivity",
+      "Negativity",
+      "Callousness",
+      "Psychopathy",
+      "ConflictedRelationships",
+      "NervousAnxiety",
+      "Inferiority",
+      "Planfulness",
+      "PracticalVsImaginative",
+      "Impulse Control",
+      "Caution",
+      "Orderliness",
+      "Detail Conscious",
+      "Discipline",
+      "Spontaneity",
+      "Competence",
+      "Unconventionality",
+      "Traditionalism",
+      "Intolerance",
+      "Liberalism",
+      "Punitiveness",
+      "Simplicity",
+      "Dishonest-Opportunism",
+      "Novelty Seeking",
+      "Resilience",
+      "Endurance",
+      "AchievementStriving",
+      "Sensitivity",
+      "ToleranceForAmbiguity",
+      "Prejudice",
+      "Abstractness",
+      "Curiosity",
+      "AestheticInterests",
+      "CognitiveInterests",
+      "Creativity",
+      "Absorption",
+      "Fantasy",
+      "Perserverance",
+      "Anxiety",
+      "Worry",
+      "Depression",
+      "Rumination",
+      "Anger",
+      "Embarrassment"
+    ]
+  
+  var chosen_list = []
+
+  var i;
+  for (i = 0; i < tnames.length; i++) {
+    if (vals[i] > thresh) {
+      chosen_list.push(tnames[i])
+    }
+  }
+  return (chosen)
+}
+render() {
+  vals = list_of_virtues()
+  const listItems = vals.map((number) =>
+    <li>{number}</li>
+  return (
+    <div>
+      "In the darkest night of your life, these are the virtues that will assist you."
+      <ul>{listitems}</ul>
+    </div>
+  )
+}
 }
 
 class IndividualMyth extends Component {
